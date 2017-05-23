091049d6424dace00438108cd1e47e583f41f1da
Miguel Pena <miguelp1986@gmail.com>
2017-05-23 10:31:07 -0700

    made changes from class lecture

--- src/client.py
+++ src/client.py
@@ -1,34 +1,29 @@
 """This is the client socket for our echo application."""
 
 import socket
+import sys
 
 def client(message):
     """."""
     address_info = socket.getaddrinfo('127.0.0.1', 5000)
-
     stream_info = [i for i in address_info if i[1] == socket.SOCK_STREAM][0]
-
     client = socket.socket(*stream_info[:3])
-
     client.connect(stream_info[-1])
-
     client.sendall(message.encode('utf8'))
 
-    # receive data
-
-    buffer_length = 8
+    buffer_length = 64
     reply_complete = False
+    message = b''
 
     while not reply_complete:
         part = client.recv(buffer_length)
-        print(part.decode('utf8'))
+        message += part
 
         if len(part) < buffer_length:
+            print(message.decode('utf8'))
             break
-
 
 if __name__ == '__main__':
     """."""
-    import sys
     message = sys.argv[1]
     client(message)
--- src/server.py
+++ src/server.py
@@ -1,35 +1,41 @@
 """This is the server socket for our echo application."""
 
 import socket
+import sys
 
 
 def server():
     """."""
     server = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM, socket.IPPROTO_TCP)
-
     address = ('127.0.0.1', 5000)
-
     server.bind(address)
-
     server.listen(1)
 
-    connection, address = server.accept()
+    while True:
+        try:
+            connection, address = server.accept()
 
-    buffer_length = 8
-    message_complete = False
-    while not message_complete:
-        part = connection.recv(buffer_length)
-        print(part.decode('utf8'))
+            buffer_length = 64
+            message_complete = False
+            message = b''
+            while not message_complete:
+                part = connection.recv(buffer_length)
+                message += part
 
-        if len(part) < buffer_length:
-            break
+                if len(part) < buffer_length:
+                    break
 
-    message = "I hear you, loud and clear!"
-    connection.sendall(message.encode('utf8'))
+            connection.sendall(message)
+            connection.close()
+
+        except KeyboardInterrupt:
+            server.close()
+            print('Shutting down echo server...')
+            sys.exit()
 
 
 if __name__ == '__main__':
     """."""
-    import sys
+    print('Your echo server is up and running')
     server()
