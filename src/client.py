54d597341d6eb237293329fa1612978bc9b669bc
Miguel Pena <miguelp1986@gmail.com>
2017-05-24 10:06:56 -0700

    changes to server and client

--- src/client.py
+++ src/client.py
@@ -9,7 +9,7 @@ def client(message):
     stream_info = [i for i in address_info if i[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
-    message += '*'
+    message += '\r\n\r\n'
     client.sendall(message.encode('utf8'))
 
     buffer_length = 8
@@ -19,9 +19,10 @@ def client(message):
         part = client.recv(buffer_length).decode()
         message += part
 
-        if '*' in message:
+        if message.endswith('\r\n\r\n'):
             break
 
+    client.shutdown(socket.SHUT_WR)
     client.close()
     print(message[:-1])
     return message[:-1]
