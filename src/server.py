0ce0ea40efa62e1d94b40dc6ffe5517cfc634211
Miguel Pena <miguelp1986@gmail.com>
2017-05-25 13:43:54 -0700

    changes to server and client

--- src/server.py
+++ src/server.py
@@ -18,7 +18,7 @@ def server():
     """."""
     server = socket.socket(socket.AF_INET,
                            socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    address = ('127.0.0.1', 5000)
+    address = ('127.0.0.1', 5001)
     server.bind(address)
     server.listen(1)
 
