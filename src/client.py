3875a027067b785dda8172e1133506085edf7ff8
ADL175 <armydavidlim@gmail.com>
2017-05-25 09:46:46 -0700

    trying to get to branch

--- src/client.py
+++ src/client.py
@@ -1,32 +1,43 @@
-"""This is the client socket for our echo application."""
-
+# -*- coding: utf-8 -*-
+"""Client for the http server assignment."""
 import socket
 import sys
 
-def client(message):
-    """."""
-    address_info = socket.getaddrinfo('127.0.0.1', 5000)
-    stream_info = [i for i in address_info if i[1] == socket.SOCK_STREAM][0]
+
+def main(words):  # pragma: no cover
+    """
+    Joins list of words from system args, then runs start_client with them.
+    """
+    if words is not '':
+        print(start_client(' '.join(words)))
+
+
+def start_client(msg):
+    """
+    Adds a signature character to the end of the message. Connects to server and sends
+    string message. Then parses the response using the special character. Removes character
+    to output response.
+    """
+    msg = msg + '§'
+    if sys.version_info.major == 2:
+        msg = msg.decode('utf8')
+    addr_info = socket.getaddrinfo('127.0.0.1', 5015)
+    stream_info = [attr for attr in addr_info if attr[1] == socket.SOCK_STREAM][0]
     client = socket.socket(*stream_info[:3])
     client.connect(stream_info[-1])
-    message += '*'
-    client.sendall(message.encode('utf8'))
-
-    buffer_length = 8
-    message = ''
-
-    while True:
-        part = client.recv(buffer_length).decode()
-        message += part
-
-        if '*' in message:
-            break
-
+    client.sendall(msg.encode('utf8'))
+    flag = True
+    res = b""
+    while flag is True:
+        more = client.recv(8)
+        res += more
+        if res[-1:] == b"\xa7":
+            flag = False
     client.close()
-    print(message[:-1])
-    return message[:-1]
+    if sys.version_info.major == 2:
+        return res[:-2]
+    return res.decode('utf8')[:-1]
 
-if __name__ == '__main__': #pragma: no cover
-    """."""
-    message = sys.argv[1]
-    client(message)
+if __name__ == '__main__':#  pragma: no cover
+    if len(sys.argv) > 1:
+        main(sys.argv[1:])
--- src/server.py
+++ src/server.py
@@ -1,60 +1,43 @@
-"""This is the server socket for our echo application."""
+# -*- coding: utf-8 -*-
+"""Server for http server assignment."""
 
 import socket
 import sys
 
 
-def response_ok():
-    response = b'HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nSuccess*'
-    return response
-
-
-def response_error():
-    return """
-HTTP/1.1 500 Internal Server Error
-Content-Type: text/plain
-Server Error"""
-
-
-def server():
-    """."""
-    server = socket.socket(socket.AF_INET,
-                           socket.SOCK_STREAM, socket.IPPROTO_TCP)
-    address = ('127.0.0.1', 5000)
+def main():  # pragma: no cover
+    """Main server loop. Logs data into log variable until it finds a certain character. Then returns response."""
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM, socket.IPPROTO_TCP)
+    address = ('127.0.0.1', 5015)
     server.bind(address)
     server.listen(1)
-
     while True:
         try:
-
             connection, address = server.accept()
-
-            buffer_length = 8
-            message_complete = False
-            message = ''
-
-            while not message_complete:
-                print(message)
-                part = connection.recv(buffer_length).decode()
-                message += part
-
-                if len(part) < buffer_length:
-                    #break
-                    response = response_ok()
-                    # print(response)
-                    # print(message[:-1])
-                    # print(response_ok())
-                    connection.sendall(response)
-                    connection.close()
-                    break
-
+            log = b""
+            flag = True
+            while flag is True:
+                more = connection.recv(8)
+                log += more
+                if log[-1:] == b"\xa7":
+                    flag = False
+            sys.stdout.write(log.decode('utf8')[:-1])
+            response = response_ok()
+            connection.sendall(response)
+            connection.close()
         except KeyboardInterrupt:
             server.close()
-            print('Shutting down echo server...')
             sys.exit()
 
+def response_ok():#  pragma: no cover
+    response = b"HTTP/1.1 200 OK \r\nContent-Type: text/plain \r\n \xc2\xa7"
+    """Returns 200 response."""
+    return response
 
-if __name__ == '__main__': # pragma: no cover
-    """."""
-    print('Your echo server is up and running')
-    server()
+def response_error():
+    """Returns 500 response."""
+    response = b"HTTP/1.1 500 Internal Server Error \r\nContent-Type: text/plain \r\n \xc2\xa7"
+    return response
+
+if __name__== '__main__':#  pragma: no cover
+    main()
